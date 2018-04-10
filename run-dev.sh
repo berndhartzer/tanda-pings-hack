9db56cbb0ad6b434369316583221c49da3a913ee
Bernd <berndhartzer@gmail.com>
2018-04-10 21:33:00 +1000

    updated docker run script to not run detached

--- run-dev.sh
+++ run-dev.sh
@@ -2,4 +2,5 @@
 
 CONTAINER=$1
 
-docker run -v $(pwd):/var/www -d -p 3000:80 $CONTAINER
+# docker run -v $(pwd):/var/www -d -p 3000:80 $CONTAINER
+docker run -v $(pwd):/var/www -p 3000:80 $CONTAINER
