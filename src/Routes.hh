afee5befb77a0be08c6d31edb964173aa81d0da9
Bernd <berndhartzer@gmail.com>
2018-04-14 22:46:59 +1000

    started implementing solution

--- src/Routes.hh
+++ src/Routes.hh
@@ -11,13 +11,49 @@ class Routes implements RoutesInterface {
   public function registerRoutes(RouterInterface $router): void {
 
     $router->get('/', (RequestInterface $request): \JsonSerializable ==> {
-      return new \Something\One\HomeRoute();
+      return new \Pings\Handlers\HomeHandler();
     });
 
     $router->get('/get_some/:id', (RequestInterface $request): \JsonSerializable ==> {
-      $route = new \Something\One\GetSomeIdRoute($request);
+      $route = new \Pings\Handlers\GetSomeIdHandler($request);
       $route->doSomething();
       return $route;
+    });
+
+    $router->get('/something', (RequestInterface $request): \JsonSerializable ==> {
+      $route = new \Pings\Handlers\SomeNewHandler();
+      $route->handle();
+      return $route;
+    });
+
+    $router->get('/:device_id/:date', (RequestInterface $request): \JsonSerializable ==> {
+      return new \Pings\Handlers\HomeHandler();
+    });
+
+    $router->get('/:device_id/:from/:to', (RequestInterface $request): \JsonSerializable ==> {
+      return new \Pings\Handlers\HomeHandler();
+    });
+
+    $router->get('/all/:date', (RequestInterface $request): \JsonSerializable ==> {
+      return new \Pings\Handlers\HomeHandler();
+    });
+
+    $router->get('/all/:from/:to', (RequestInterface $request): \JsonSerializable ==> {
+      return new \Pings\Handlers\HomeHandler();
+    });
+
+    $router->get('/devices', (RequestInterface $request): \JsonSerializable ==> {
+      return new \Pings\Handlers\DevicesHandler();
+    });
+
+    $router->post('/:device_id/:epoch_time', (RequestInterface $request): \JsonSerializable ==> {
+      $response = new \Pings\Handlers\PingHandler($request);
+      $response->handle();
+      return $response;
+    });
+
+    $router->post('/clear_data', (RequestInterface $request): \JsonSerializable ==> {
+      return new \Pings\Handlers\HomeHandler();
     });
 
   }
--- src/handlers/Something.hh
+++ src/handlers/Something.hh
@@ -1,17 +1,17 @@
 <?hh // strict
 
-namespace Something\One;
+namespace Pings\Handlers;
 
 use Usox\Sharesta\RequestInterface;
 
-class HomeRoute implements \JsonSerializable {
+class HomeHandler implements \JsonSerializable {
 
   public function jsonSerialize(): string {
     return 'Root route';
   }
 }
 
-class GetSomeIdRoute implements \JsonSerializable {
+class GetSomeIdHandler implements \JsonSerializable {
 
   private ?string $request_id;
 
@@ -26,4 +26,53 @@ class GetSomeIdRoute implements \JsonSerializable {
   public function jsonSerialize(): string {
     return 'Got id: '.(string) $this->request_id;
   }
+}
+
+class SomeNewHandler implements \JsonSerializable {
+
+  private ?dict $some_dict;
+
+  public function handle(): void {
+    $this->some_dict = dict['a' => 'one', 'b' => 'two'];
+  }
+
+  public function jsonSerialize(): dict {
+    return $this->some_dict;
+  }
+
+}
+
+class DevicesHandler implements \JsonSerializable {
+
+  public function handle(): void {
+  }
+
+  public function jsonSerialize(): Vector {
+    return new Vector(null);
+  }
+
+}
+
+class PingHandler implements \JsonSerializable {
+
+  public function __construct(private RequestInterface $request): void {
+
+  }
+
+  public function handle(): void {
+    $device_id = $this->request->getRouteParameters()->get('device_id');
+    $epoch_time = $this->request->getRouteParameters()->get('epoch_time');
+
+    $myfile = fopen('/var/www/newfile.txt', 'a') or die('Unable to open file!');
+    $txt = $device_id . ' - ' . $epoch_time . '\n';
+    fwrite($myfile, $txt);
+    fclose($myfile);
+
+
+  }
+
+  public function jsonSerialize(): string {
+    return 'ok';
+  }
+
 }
