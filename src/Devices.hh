a43cc3378266898a5d6272334f3f2af397c8d4e4
Bernd Hartzer <berndhartzer@gmail.com>
2018-05-07 19:13:53 +1000

    big refactor of app

---
+++ src/Devices.hh
@@ -0,0 +1,127 @@
+<?hh // strict
+
+namespace Pings\Devices;
+
+use Pings\Db\Db;
+
+class Devices {
+
+  public function __construct(): void {
+
+  }
+
+  public function getDevices(): Set<string> {
+
+    $devices = new Set();
+
+    $db = new Db();
+    $stmt = $db->query('SELECT DISTINCT device_id FROM pings');
+
+    while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+      $devices->add($row['device_id']);
+    }
+
+    return $devices;
+  }
+
+  public function getDevicePingsOnDate($device_id, $date): Vector<int> {
+
+    $time_from = strtotime($date);
+    $time_to = strtotime('+1 day', $time_from);
+
+    return $this->getDevicePings($device_id, $time_from, $time_to);
+  }
+
+  public function getDevicePingsInRange($device_id, $from, $to): void {
+
+    $time_from = $this->dateIsTimestamp($from) ? $from : strtotime($from);
+    $time_to = $this->dateIsTimestamp($to) ? $to : strtotime($to);
+
+    return $this->getDevicePings($device_id, $time_from, $time_to);
+  }
+
+  public function getAllPingsOnDate($date): Map<string, Vector> {
+
+    $time_from = strtotime($date);
+    $time_to = strtotime('+1 day', $time_from);
+
+    return $this->getAllPings($time_from, $time_to);
+  }
+
+  public function getAllPingsInRange($from, $to): Map<string, Vector> {
+
+    $time_from = $this->dateIsTimestamp($from) ? $from : strtotime($from);
+    $time_to = $this->dateIsTimestamp($to) ? $to : strtotime($to);
+
+    return $this->getAllPings($time_from, $time_to);
+  }
+
+  public function addPing($device_id, $timestamp): Map<string, string> {
+
+    $query_params = Map {
+      ':device_id' => $device_id,
+      ':timestamp' => $timestamp
+    };
+
+    $db = new Db();
+    $db->query('INSERT INTO pings (device_id, timestamp) VALUES (:device_id, :timestamp)', $query_params);
+
+    return $query_params;
+  }
+
+  public function deleteAllPings(): void {
+    $db = new Db();
+    $db->query('DELETE FROM pings');
+  } 
+
+  private function getDevicePings($device_id, $time_from, $time_to): Vector<int> {
+
+    $pings = new Vector();
+
+    $query_params = Map {
+      ':device_id' => $device_id,
+      ':time_from' => $time_from,
+      ':time_to' => $time_to
+    };
+
+    $db = new Db();
+
+    $stmt = $db->query('SELECT timestamp FROM pings WHERE device_id = :device_id AND timestamp >= :time_from AND timestamp < :time_to', $query_params);
+
+    while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+      $pings->add((int)$row['timestamp']);
+    }
+
+    return $pings;
+  }
+
+  private function getAllPings($time_from, $time_to): Map<string, Vector> {
+
+    $pings = new Map();
+
+    $query_params = Map {
+      ':time_from' => $time_from,
+      ':time_to' => $time_to
+    };
+
+    $db = new Db();
+
+    $stmt = $db->query('SELECT device_id, timestamp FROM pings WHERE timestamp >= :time_from AND timestamp < :time_to', $query_params);
+
+    while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+
+      if (!isset($pings[$row['device_id']])) {
+        $pings[$row['device_id']] = new Vector();
+      }
+
+      $pings[$row['device_id']]->add((int)$row['timestamp']);
+    }
+
+    return $pings;
+  }
+
+  private function dateIsTimestamp(string $date): Boolean {
+    return is_numeric($date);
+  }
+
+}
---
+++ src/Handlers.hh
@@ -0,0 +1,149 @@
+<?hh // strict
+
+namespace Pings\Handlers;
+
+use Usox\Sharesta\RequestInterface;
+use Pings\Devices\Devices;
+
+class DeviceHandler implements \JsonSerializable {
+
+  private string $device_id;
+  private Vector<int> $response;
+  private string $from;
+  private string $to;
+
+  public function __construct(private RequestInterface $request): void {
+
+    $this->response = new Vector();
+    $this->device_id = $request->getRouteParameters()->get('device_id');
+
+    if (isset($request->getRouteParameters()->get('date'))) {
+
+      $this->from = $request->getRouteParameters()->get('date');
+      $this->to = null;
+
+    } else if (isset($request->getRouteParameters()->get('from'))) {
+
+      $this->from = $request->getRouteParameters()->get('from');
+      $this->to = $request->getRouteParameters()->get('to');
+
+    }
+  }
+
+  public function handle(): void {
+
+    $devices = new Devices();
+
+    if ($this->to === null) {
+      $device_pings = $devices->getDevicePingsOnDate($this->device_id, $this->from);
+    } else {
+      $device_pings = $devices->getDevicePingsInRange($this->device_id, $this->from, $this->to);
+    }
+
+    $this->response = $device_pings;
+  }
+
+  public function jsonSerialize(): Vector<int> {
+    return $this->response;
+  }
+
+}
+
+class AllHandler implements \JsonSerializable {
+
+  private string $from;
+  private string $to;
+  private Map<string, Vector> $response;
+
+  public function __construct(private RequestInterface $request): void {
+
+    $this->response = new Map();
+
+    if (isset($request->getRouteParameters()->get('date'))) {
+
+      $this->from = $request->getRouteParameters()->get('date');
+      $this->to = null;
+
+    } else if (isset($request->getRouteParameters()->get('from'))) {
+
+      $this->from = $request->getRouteParameters()->get('from');
+      $this->to = $request->getRouteParameters()->get('to');
+
+    }
+
+  }
+
+  public function handle(): void {
+
+    $devices = new Devices();
+
+    if ($this->to === null) {
+      $all_pings = $devices->getAllPingsOnDate($this->from);
+    } else {
+      $all_pings = $devices->getAllPingsInRange($this->from, $this->to);
+    }
+
+    $this->response = $all_pings;
+  }
+
+  public function jsonSerialize(): Map<string, Vector> {
+    return $this->response;
+  }
+
+}
+
+class DevicesHandler implements \JsonSerializable {
+
+  private Set<string> $response;
+
+  public function __construct(): void {
+    $this->response = new Set();
+  }
+
+  public function handle(): void {
+    $devices = new Devices();
+    $this->response = $devices->getDevices();
+  }
+
+  public function jsonSerialize(): Set<string> {
+    return $this->response;
+  }
+
+}
+
+class PingHandler implements \JsonSerializable {
+
+  private string $device_id;
+  private int $timestamp;
+  private Map<string, string> $response;
+
+  public function __construct(private RequestInterface $request): void {
+    $this->device_id = $request->getRouteParameters()->get('device_id');
+    $this->timestamp = $request->getRouteParameters()->get('epoch_time');
+  }
+
+  public function handle(): void {
+    $devices = new Devices();
+    $this->response = $devices->addPing($this->device_id, $this->timestamp);
+  }
+
+  public function jsonSerialize(): Map<string, string> {
+    return $this->response;
+  }
+
+}
+
+class ClearHandler implements \JsonSerializable {
+
+  private string $response;
+
+  public function handle(): void {
+    $devices = new Devices();
+    $devices->deleteAllPings();
+    $this->response = 'ok';
+  }
+
+  public function jsonSerialize(): string {
+    return $this->response;
+  }
+}
